85de9ef5eb4df0e53ed034f12da781ef073d2d5d
Niko Mikulicic <niko.mikulicic@gmail.com>
2018-02-05 17:40:54 +0100

    Location implements NSCoding

--- PickupRide/Store/Models/Location.swift
+++ PickupRide/Store/Models/Location.swift
@@ -9,7 +9,12 @@
 import Foundation
 import CoreLocation
 
-class Location: NSObject, Codable {
+private enum PropertyKey: String {
+    case latitude
+    case longitude
+}
+
+class Location: NSObject, NSCoding {
     let latitude: CLLocationDegrees
     let longitude: CLLocationDegrees
 
@@ -23,4 +28,18 @@ class Location: NSObject, Codable {
         self.init(latitude: location.coordinate.latitude,
                   longitude: location.coordinate.longitude)
     }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard
+            let latitude = aDecoder.decodeObject(forKey: PropertyKey.latitude.rawValue) as? Double,
+            let longitude = aDecoder.decodeObject(forKey: PropertyKey.longitude.rawValue) as? Double
+        else { return nil }
+        self.init(latitude: latitude, longitude: longitude)
+    }
+    
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(latitude, forKey: PropertyKey.latitude.rawValue)
+        aCoder.encode(longitude, forKey: PropertyKey.longitude.rawValue)
+    }
+
 }
